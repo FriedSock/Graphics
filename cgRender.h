96a83077738f7470a14a3d1b4f2251d243654cb4
Jack Bracewell <jbtwentythree@gmail.com>
2013-02-19 20:05:52 +0000

    init

---
+++ cgRender.h
@@ -0,0 +1,19 @@
+#ifndef _CG_RENDER_H
+
+#define _CG_RENDER_H
+
+#include <GL/glut.h>
+#include <stdlib.h>
+
+using namespace std;
+
+#include <fstream>
+#include <iostream>
+
+#endif
+
+struct point {
+    float x;
+    float y;
+    float z;
+};
